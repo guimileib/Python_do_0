8e139939f1629264842561375b1a73e0ec2a3b67
guimileib <guilhermemileib@gmail.com>
2025-09-24 11:19:40 -0300

    feat: Day 7

---
+++ 07/list.py
@@ -0,0 +1,17 @@
+'''
+EX1:
+Escreva um programa que use um laço for com a função range() para exibir os números de 1 a 10.
+
+EX2:
+Crie uma lista com pelo menos 3 tarefas que você precisa fazer (por exemplo: ["Estudar Python", "Fazer compras", "Lavar a louça"]).
+ Em seguida, use um laço for para exibir cada tarefa, uma por linha.
+'''
+
+
+'''for numero in range(1, 11):
+    print(numero)'''
+
+todo = ['Estudar Python', 'Fazer trabalho de PPI', 'Exercitar', 'Projeto Pessoal', 'Sprint']
+
+for task in todo:
+    print(task)
